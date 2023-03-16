4b7388f7a502258f653df81c142d341cb3abc09c
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-03-16 14:02:50 -0600

    feat: display info message

--- main.asm
+++ main.asm
@@ -1,13 +1,78 @@
+; Program Description: Juego de damas chinas en ensamblador
+; Author: Daniel Estuardo Cuque Ruiz
+; Creation Date: 14 de marzo de 2023
+
+
+; ------------------------------------------------
+; Imprimir
+; esta macro se encarga de imprimir una cadena de caracteres en la pantalla. Para ello, primero carga la dirección de 
+; inicio de la sección de datos en el registro AX y luego la carga en el registro DS, lo que permite acceder a la 
+; memoria de la cadena. Luego, carga el carácter de impresión de cadena en el registro AH y la dirección de inicio de 
+; la cadena en el registro DX, y finalmente llama a la interrupción 21h para imprimir la cadena.
+; ------------------------------------------------
+printMsg macro str
+              mov ax, @data          ; ax = offset de la cadena
+              mov ds, ax             ; ds = segmento de la cadena
+              mov ah, 09h            ; imprimir cadena
+              lea dx, str     ; dx = offset de la cadena
+              int 21h                ; imprimir cadena
+endm
+
+; ------------------------------------------------
+; Imprimir un caracter ascii
+; esta macro se encarga de imprimir un carácter ASCII en la pantalla. Para ello, primero carga la dirección de inicio 
+; de la sección de datos en el registro AX y luego la carga en el registro DS, lo que permite acceder a la memoria de 
+; la cadena. Luego, carga el carácter de impresión de carácter en el registro AH y el carácter ASCII en el registro DL, 
+; y finalmente llama a la interrupción 21h para imprimir el carácter.
+; ------------------------------------------------
+printAscii macro ascii
+                mov ax, @data     ; ax = offset de la cadena
+                mov ds, ax        ; ds = segmento de la cadena
+                mov ah, 02h       ; imprimir caracter
+                mov dl, ascii     ; dx = offset de la cadena
+                int 21h           ; imprimir cadena
+endm
+
+; Configuración del programa
 .MODEL small
 .STACK
-.RADIX 16
+.RADIX 16 ; para que los números se muestren en hexadecimal
 .DATA
 
+; Creamos la información de incio, seguido de la espera de ENTER para pasar al menú
+infoMsg DB 'Universidad de San Carlos de Guatemala', 0Dh, 0Ah,
+     'Facultad de Ingenieria', 0Dh, 0Ah,
+     'Escuela de Ciencias y Sistemas', 0Dh, 0Ah,
+     'Arquitectura de computadores y ensambladores 1', 0Dh, 0Ah,
+     'Seccion B', 0Dh, 0Ah,
+     'Daniel Estuardo Cuque Ruiz' , 0Dh, 0Ah,
+     '202112145' , 0Dh, 0Ah,
+     'ENTER para continuar' , 0Dh, 0Ah, '$'
+
+; Creamos la información del menú
+menuMessage DB 'Menu:', 0Dh, 0Ah,
+     '1. Iniciar', 0Dh, 0Ah,
+     '2. Cargar partida', 0Dh, 0Ah,
+     '3. Salir', 0Dh, 0Ah, '$'
+
+
+; Iniciamos el bloque de código
 .CODE
-inicio: 
-    main proc
-        mov al, 0
-        mov ah, 04ch
-        int 21
-    main endp
-END inicio
+start:
+main PROC
+
+mov ax, @data
+mov ds, ax
+
+; Imprimimos la información de inicio
+printMsg infoMsg
+
+; Esperamos a que se presione ENTER
+mov ah, 08h
+int 21h
+
+
+main ENDP
+END start
+
+
