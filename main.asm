7a916def69684547abb73405f0f00e1b722736cb
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-03-16 16:12:10 -0600

    feat: select menu option

--- main.asm
+++ main.asm
@@ -47,6 +47,7 @@ infoMsg DB 'Universidad de San Carlos de Guatemala', 0Dh, 0Ah,
      'Seccion B', 0Dh, 0Ah,
      'Daniel Estuardo Cuque Ruiz' , 0Dh, 0Ah,
      '202112145' , 0Dh, 0Ah,
+     0Dh, 0Ah,
      'ENTER para continuar' , 0Dh, 0Ah, '$'
 
 ; Creamos la información del menú
@@ -55,22 +56,63 @@ menuMessage DB 'Menu:', 0Dh, 0Ah,
      '2. Cargar partida', 0Dh, 0Ah,
      '3. Salir', 0Dh, 0Ah, '$'
 
+; ------------------------------------------------
+; Variables para la option_1 de inicio de juego
+; ------------------------------------------------
+t1 DB 'op1', 0Dh, 0Ah, '$'
+
+; ------------------------------------------------
+; Variables para la option_2 de inicio de juego
+; ------------------------------------------------
+t2 DB 'op2', 0Dh, 0Ah, '$'
 
 ; Iniciamos el bloque de código
 .CODE
 start:
 main PROC
 
+; Cargamos la dirección de inicio de la sección de datos en el registro AX y luego la cargamos en el registro DS,
 mov ax, @data
 mov ds, ax
 
-; Imprimimos la información de inicio
-printMsg infoMsg
-
-; Esperamos a que se presione ENTER
-mov ah, 08h
+mov ah, 09h             ; Imprimimos la información de inicio
+lea dx, infoMsg
 int 21h
 
+mov ah, 08h             ; Esperamos a que se presione ENTER
+int 21h
+
+menu:                   ; Creamos un label para el menú
+
+printMsg menuMessage    ; Imprimimos el menú
+
+mov ah, 00h             ; Si no es ninguna de las opciones, volvemos a imprimir el menú
+int 16h
+
+cmp al, 31              ; 31 es el código ASCII de 1
+je option_1             ; Si es 1, saltamos a la opción 1
+cmp al, 32              ; 32 es el código ASCII de 2
+je option_2             ; Si es 2, saltamos a la opción 2
+cmp al, 33              ; 33 es el código ASCII de 3
+je option_3             ; Si es 3, saltamos a la opción 3
+
+jmp menu                ; Si no es ninguna de las opciones, volvemos a imprimir el menú
+
+
+option_3:               ; Colocamos la salida del programa
+jmp exit                ; Llamamos a la rutina de salida
+
+option_1:               ; Llamamos a la rutina de inicio de juego
+printMsg t1
+jmp exit
+
+option_2:               ; Llamamos a la rutina de inicio de juego
+printMsg t2
+jmp exit
+
+exit:
+mov ah, 4Ch
+int 21h
 
 main ENDP
 END start
